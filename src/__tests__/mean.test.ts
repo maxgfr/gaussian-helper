c871cb40c8c121808cbf2e1d7d6f02fec6d23504
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-12-08 00:58:01 +0100

    fix: tu

--- src/__tests__/mean.test.ts
+++ src/__tests__/mean.test.ts
@@ -2,6 +2,6 @@ import { getMean } from '../mean';
 
 describe('getMean', () => {
   it('should get mean', () => {
-    expect(getMean([10, 2, 3, 4, 5])).toBe(5);
+    expect(getMean([10, 2, 3, 4, 5])).toBe(4.800000000000001);
   });
 });
